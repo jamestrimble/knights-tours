e601be906174733614b604f4d8ea077d513a6b82
James Trimble <james.trimble@yahoo.co.uk>
2021-12-14 11:37:02 +0000

    Generalise algorithm

--- p2pk.py
+++ p2pk.py
@@ -1,74 +1,82 @@
 import sys
 
 n = int(sys.argv[1])
-k = int(sys.argv[2])
+knights_len = int(sys.argv[2])
+k = int(sys.argv[3])
 
-steps = [-2, -1, 1, 2]
-moves = []
-for x in steps:
-    for y in steps:
-        if abs(x) + abs(y) == 3:
-            moves.append((x, y))
+moves = [(-2,-1), (-2,1), (-1,-2), (-1,2), (1,-2), (1,2), (2,-1), (2,1)]
 
-def update_knights(knights, move):
+def valid_position(knight, square_used):
+    return (
+        knight[0] >= 0 and
+        knight[0] < n and
+        knight[1] >= 0 and
+        knight[1] < n and
+        not square_used[knight[0]][knight[1]]
+    )
+
+def revert_knights(knights, move, square_used):
     for knight in knights:
-        knight[0] += move[0]
-        knight[1] += move[1]
-
-def revert_knights(knights, move):
-    for knight in knights:
+        square_used[knight[0]][knight[1]] = 0
         knight[0] -= move[0]
         knight[1] -= move[1]
 
-def valid_positions(knights, square_used):
-    for knight in knights:
-        if knight[0] < 0 or knight[0] >= n:
+def update_knights(knights, move, square_used, value):
+    for i, knight in enumerate(knights):
+        if not valid_position([knight[0] + move[0], knight[1] + move[1]], square_used):
+            revert_knights(knights[:i], move, square_used)
             return False
-        if knight[1] < 0 or knight[1] >= n:
-            return False
-        if square_used[knight[0]][knight[1]]:
-            return False
+        knight[0] += move[0]
+        knight[1] += move[1]
+        set_square(square_used, knight, (value, i))
     return True
 
 def set_square(square_used, knight, value):
     square_used[knight[0]][knight[1]] = value
 
-def unset_square(square_used, knight):
-    square_used[knight[0]][knight[1]] = 0
-
 def search(knights, square_used, tour_len):
     if tour_len == k:
-        return square_used
+        return True
     for move in moves:
-        update_knights(knights, move)
-        if valid_positions(knights, square_used):
-            tour_len += 1
-            set_square(square_used, knights[0], tour_len)
-            set_square(square_used, knights[1], tour_len + 1000)
-            if search(knights, square_used, tour_len):
-                return square_used
-            unset_square(square_used, knights[0])
-            unset_square(square_used, knights[1])
-            tour_len -= 1
-        revert_knights(knights, move)
+        if update_knights(knights, move, square_used, tour_len + 1):
+            if search(knights, square_used, tour_len + 1):
+                return True
+            revert_knights(knights, move, square_used)
     return False
+
+def initial_knights(square_used, knights_len, knights):
+    if len(knights) == 0:
+        for i in range(n):
+            for j in range(n):
+                set_square(square_used, [i, j], (1, 0))
+                if knights_len == 1:
+                    yield [i, j]
+                else:
+                    yield from initial_knights(square_used, knights_len, [[i, j]])
+                square_used[i][j] = 0
+    else:
+        for move in moves if len(knights) > 1 else [(1, 2)]:
+            prev_knight = knights[-1]
+            new_knight = [prev_knight[0] + move[0], prev_knight[1] + move[1]]
+            if valid_position(new_knight, square_used):
+                knights.append(new_knight)
+                set_square(square_used, new_knight, (1, len(knights)))
+                if knights_len == len(knights):
+                    yield knights
+                else:
+                    yield from initial_knights(square_used, knights_len, knights)
+                square_used[new_knight[0]][new_knight[1]] = 0
+                del knights[-1]
 
 def find_tour():
     square_used = [[0] * n for _ in range(n)]
-    for i in range(1, n):
-        for j in range(2, n):
-            knights = [[i-1, j-2], [i, j]]
-            set_square(square_used, knights[0], 1)
-            set_square(square_used, knights[1], 1001)
-            result = search(knights, square_used, 1)
-            if result:
-                return result
-            unset_square(square_used, knights[0])
-            unset_square(square_used, knights[1])
+    for knights in initial_knights(square_used, knights_len, []):
+        if search(knights, square_used, 1):
+            return square_used
 
 result = find_tour()
 if result:
     for row in result:
-        print(' '.join('{: >3}'.format(str(x) + ' ' if x < 1000 else str(x-1000) + "'") for x in row))
+        print(' '.join('{: >7}'.format(str(x)) for x in row))
 else:
     print("No solution exists")
